8b351c9c0ed49f2d944c4f150bef7c8ee757cee2
Uzair <uzairmahboob1@gmail.com>
2023-08-23 13:56:06 -0400

    g

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
 import Navbar from './Nav';
-import Background from './Background';
 import ProjectsSection from './ProjectsSection';
 import AboutSection from './AboutSection';
 import Footer from './Footer';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import spacebg from './spacebg.jpg';
+import Background from './Background';
 
 const App = () => {
   return (
     <div className="App">
       <Background />
-      <Navbar />
-      <div className='content'>
-        <AboutSection />
-        <ProjectsSection />
-      </div>
-      <Footer />
+      <Parallax pages={2} speed={1}>
+        <ParallaxLayer stick={{start: 0, end: 1}}>
+          <Navbar />
+        </ParallaxLayer>
+        <div className='content'>
+            
+          <ParallaxLayer offset={0.3} factor={1.5} speed={0.8}>
+            <AboutSection />
+          </ParallaxLayer>
+          <ParallaxLayer offset={window.innerWidth < 800 ? 0.7 : 1} speed={0.7}>
+            <ProjectsSection />
+          </ParallaxLayer>
+          <ParallaxLayer offset={1} speed={0.5}>
+            <Footer />
+          </ParallaxLayer>
+        </div>
+      </Parallax>
     </div>
   );
 }
